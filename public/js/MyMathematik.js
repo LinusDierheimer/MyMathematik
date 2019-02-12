027571f0f505cb566d3389371f16c927252a54b7
Linus <Linus@Dierheimer.de>
2019-02-12 22:01:34 +0100

    accordion for video site

--- public/js/MyMathematik.js
+++ public/js/MyMathematik.js
@@ -1,7 +1,6 @@
-
-/***************/
-/*Sticky header*/
-/**++++++++++++*/
+$(document).resize(function(){
+    console.log(2);
+});
 
 window.onscroll = function() {
     const header = document.getElementById("header");
@@ -12,3 +11,41 @@ window.onscroll = function() {
         header.classList.remove("headersticky");
     }
 };
+
+function updatePanelWidths(){
+    const chapterbuttons = document.getElementsByClassName("chaptertitle");
+    for(var i = 0; i < chapterbuttons.length; i++){
+        const panel = chapterbuttons[i].nextElementSibling;
+        if(chapterbuttons[i].classList.contains("active"))
+            panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+}
+
+window.onload = function(){
+
+    const chapterbuttons = document.getElementsByClassName("chaptertitle");
+    for(var i = 0; i < chapterbuttons.length; i++){
+        chapterbuttons[i].addEventListener('click', function(){
+
+            const isActive = this.classList.contains("active");
+
+            const panel = this.nextElementSibling;
+            const image = this.children[1];
+            if(isActive){
+                panel.style.maxHeight = null;
+                panel.style.paddingTop = "0";
+                image.src = "/assets/arrow_down.svg";
+            }else{
+                panel.style.maxHeight = panel.scrollHeight + "px";
+                panel.style.paddingTop = "10px";
+                image.src = "/assets/arrow_up.svg";
+            }
+
+            this.classList.toggle("active");
+
+            updatePanelWidths();
+
+        });
+    }
+
+}

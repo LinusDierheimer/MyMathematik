00a256b64c32b426da07dd38fc279465a593a143
Linus Dierheimer <Linus@Dierheimer.de>
2019-06-14 16:23:30 +0200

    home has special preview image

--- assets/js/home/home.js
+++ assets/js/home/home.js
@@ -1,2 +1,15 @@
 import "../app.js";
 import "../../scss/home/home.scss";
+
+function initScrollToContent(){
+    $('#scrollToContentButton').click(function(){
+        $('html, body').animate(
+            { scrollTop: $('#content').offset().top - $('#header').height() }, 
+        'slow');
+        console.log("Hallo");
+    });
+}
+
+$(document).ready(function(){
+    initScrollToContent();
+})

24f743ef8ab3720375576d6e794f0ed2fb19ac50
Linus Dierheimer <Linus@Dierheimer.de>
2019-10-01 20:59:48 +0200

    updated createServerZip.sh

--- scripts/createServerZip.sh
+++ scripts/createServerZip.sh
@@ -1,8 +1,4 @@
-echo installing missing npm packages
-npm install
-echo install missing composer packages
-composer install
-echo building assets
-npm run build
+scripts/build.sh
+
 echo creating zip
 zip -q MyMathematik.zip -r config/ public/ src/ templates/ translations/ vendor/ .env .env.test composer.json

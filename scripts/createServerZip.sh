77250a90d685f0d862e63c5293ca97a943fcbc6c
Linus Dierheimer <Linus@Dierheimer.de>
2019-10-17 17:37:03 +0200

    reworked media paths

--- scripts/createServerZip.sh
+++ scripts/createServerZip.sh
@@ -1,4 +1,6 @@
 scripts/build.sh
 
+npm run build
+
 echo "creating zip..."
 zip -q MyMathematik.zip -FS -r config/ public/ src/ templates/ translations/ vendor/ .env .env.test composer.json
--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,6 @@ Encore
     .setOutputPath("public/build/")
     .setPublicPath("/build")
     .addEntry("app", "./assets/js/app.js")
-    .copyFiles({
-        from: "./assets/images",
-        to: "images/[path][name].[ext]"
-    })
-    .copyFiles({
-        from: "./assets/videos",
-        to: "videos/[path][name].[ext]"
-    })
-    .copyFiles({
-        from: "./assets/sponsorlogos",
-        to: "sponsorlogos/[path][name].[ext]"
-    })
     .cleanupOutputBeforeBuild()
     .enableSassLoader()
     .autoProvidejQuery()
